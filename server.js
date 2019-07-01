4fd6435bb3c6710421e4e7abb97891fdfca02677
Mark Hegedus <hmark33@gmail.com>
2019-07-01 10:48:31 +0200

    feat(server.js): create express server, render static html page

---
+++ server.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = 4500;
+
+app.use(express.static('public'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, index.html));
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}!`);
+});
